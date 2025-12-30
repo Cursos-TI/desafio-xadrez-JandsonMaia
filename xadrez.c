410bb36df2d452f056ec427cf736626b4e8eb7e3
JandsonMaia <jandsonmaia31@gmail.com>
2025-12-30 10:27:00 -0300

    Update Aventureiro xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -6,6 +6,7 @@ int main(){
     int casas_torre = 5;
     int casas_bispo = 5;
     int casas_rainha = 8;
+    int casas_cavalo = 1;
 
     //Mover a Torre 5 casas para direita
     //Usando For
@@ -16,6 +17,8 @@ int main(){
         printf("Direita\n"); //Imprime a direção do movimento
     }
     
+        printf("\n");
+
     //Mover o Bispo 5 casas para Cima e direita
     //Usando while
         printf("Movimento do Bispo:\n");
@@ -27,6 +30,8 @@ int main(){
         i++;
     }
     
+        printf("\n");
+
     //Mover a Rainha 8 casas para Esquerda
     //Usando Do-While
     printf("Movimento da Rainha: \n");
@@ -38,6 +43,22 @@ int main(){
         i++;
 
     } while (i <= casas_rainha);
+
+    printf("\n");
+
+    //Mover o Cavalo 2 casas para Baixo e 1 para Esquerda
+    //Usando while com For aninhados
+    printf("Movimento do Cavalo: \n");
+    
+    while (casas_cavalo--)
+    {
+        for (int j = 0; j < 2; j++)
+            printf("Baixo\n");
+        {
+            printf("Esquerda\n");
+        }
+        
+    }
     
 
     return 0;
