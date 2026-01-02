a8fe7b769c3cef3a70a2ef8f1ac47e403a20828e
JandsonMaia <jandsonmaia31@gmail.com>
2026-01-02 11:32:34 -0300

    Update Mestre xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,65 +1,85 @@
 #include <stdio.h>
 
+//Movimentação da Torre
+void recursivoTorre(int casas_torre){
+    if (casas_torre > 0){
+        printf("Direita \n");
+        recursivoTorre(casas_torre - 1);
+    }
+}
+
+//Movimentação do Bispo
+void recursivoBispo(int casas_bispo){
+    if (casas_bispo <= 0)
+    return;
+
+        for(int i = 0; i < 1; i++){
+            printf("Cima\n");
+        }
+            for (int j = 0; j < 1; j++){
+                printf("Direita\n");        
+    }
+   recursivoBispo(casas_bispo -1);
+}
+
+//Movimentação da Rainha
+void recursivoRainha(int casas_rainha){
+    if (casas_rainha > 0){
+        printf("Esquerda \n");
+        recursivoRainha(casas_rainha - 1);
+    }
+}
+
 int main(){
-    int i;
-    // Quantidade de casas
-    int casas_torre = 5;
-    int casas_bispo = 5;
-    int casas_rainha = 8;
-    int casas_cavalo = 1;
 
     //Mover a Torre 5 casas para direita
-    //Usando For
-        printf("Movimento da Torre: \n");
+    //Usando a Recursividade
 
-    for (int i = 1; i <= casas_torre; i++)
-    {
-        printf("Direita\n"); //Imprime a direção do movimento
-    }
-    
-        printf("\n");
+    int quantidadeTorre = 5;
 
-    //Mover o Bispo 5 casas para Cima e direita
-    //Usando while
-        printf("Movimento do Bispo:\n");
-        i = 1;
-
-    while (i <= casas_bispo)
-    {
-        printf("Cima e Direita\n");
-        i++;
-    }
-    
-        printf("\n");
-
-    //Mover a Rainha 8 casas para Esquerda
-    //Usando Do-While
-    printf("Movimento da Rainha: \n");
-    i = 1;
-
-    do
-    {
-    printf("Esquerda\n");
-        i++;
-
-    } while (i <= casas_rainha);
+    printf("Movimento da Torre: \n");
+    recursivoTorre(quantidadeTorre);
 
     printf("\n");
 
-    //Mover o Cavalo 2 casas para Baixo e 1 para Esquerda
-    //Usando while com For aninhados
-    printf("Movimento do Cavalo: \n");
+    //Mover o Bispo 5 casas para Cima e direita
+    //Usando a Recursividade com Loops Aninhados
+
+    int quantidadeBispo = 5;
+
+    printf("Movimento do Bispo: \n");
+    recursivoBispo(quantidadeBispo);
+
+    printf("\n");
+
+    //Mover a Rainha 8 casas para Esquerda
+    //Usando a Recursividade
+
+    int quantidadeRainha = 8;
+
+    printf("Movimento da Rainha: \n");
+    recursivoRainha(quantidadeRainha);
     
-    while (casas_cavalo--)
-    {
-        for (int j = 0; j < 2; j++)
-            printf("Baixo\n");
-        {
-            printf("Esquerda\n");
-        }
-        
+    printf("\n");
+
+
+    //Mover o Cavalo 2 casas para Cima e 1 para Direita
+    //Usando loops aninhados
+   printf("Movimento do Cavalo:\n");
+
+for (int casas_cavalo = 1; casas_cavalo <= 3; casas_cavalo++) {
+
+    if (casas_cavalo <= 2) {
+        printf("Cima\n");
+        continue; 
+    }
+
+    if (casas_cavalo == 3) {
+        printf("Direita\n");
+        break;
     }
     
+}
 
     return 0;
 }
